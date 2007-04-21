03a4af100e9a6a4d107bbc4e444c62b6a38bef27
Jeffrey Mann Richardson <themann@indyfantasysports.net>
2007-04-21 15:48:24 +0000

    - ported SplashScreenWindow from Frontend-GNOME to Frontend-SWF (Yay!)

---
+++ src/Frontend-SWF/SplashScreenWindow.designer.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Smuxi.Frontend.SWF
+{
+	public partial class SplashScreenWindow
+	{
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // SplashScreenWindow
+            // 
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "SplashScreenWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+
+        }
+	}
+}
