65f25376bd8ba903062c04565dcb70d42088fa03
Jeffrey Mann Richardson <themann@indyfantasysports.net>
2007-04-22 01:35:30 +0000

    - Ported SplashScreenWindow to WPF (zero lines of C# code, yay!) - Updated VS project files for recent updates

---
+++ src/Frontend-WPF/SplashScreenWindow.xaml.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Smuxi.Frontend.WPF
+{
+    /// <summary>
+    /// Interaction logic for SplashScreenWindow.xaml
+    /// </summary>
+
+    public partial class SplashScreenWindow : System.Windows.Window
+    {
+
+        public SplashScreenWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
