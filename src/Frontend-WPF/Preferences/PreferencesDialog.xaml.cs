eefb6da9239ff863aad545971e802a7cec8f49bc
Jeffrey Mann Richardson <themann@indyfantasysports.net>
2007-11-21 01:31:50 +0000

    [Smuxi]  - upgraded Visual Studio projects to 2008 format  - Added Tango icons for toolkits without default icons
    [Frontend-WPF]
     - Added nonfunctional MainWindow and PreferencesDialog

---
+++ src/Frontend-WPF/Preferences/PreferencesDialog.xaml.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Smuxi.Frontend.Wpf
+{
+    /// <summary>
+    /// Interaction logic for PreferencesDialog.xaml
+    /// </summary>
+
+    public partial class PreferencesDialog : System.Windows.Window
+    {
+
+        public PreferencesDialog()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
