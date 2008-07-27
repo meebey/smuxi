e6c8a34627b20047b3f22f34310657e90bc2951c
Mirco Bauer <meebey@meebey.net>
2008-07-27 20:53:18 +0000

    bumped version to 0.6.1

--- src/AssemblyVersion.cs
+++ src/AssemblyVersion.cs
@@ -36,4 +36,4 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.6.0.*")]
+[assembly: AssemblyVersion("0.6.1.*")]
