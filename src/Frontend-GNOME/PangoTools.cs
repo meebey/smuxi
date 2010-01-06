09b2f31214fc59e19ba942a65d770140fb8154c1
David Paleino <dapal@debian.org>
2010-01-06 23:51:49 +0100

    Show color and formattings of topics in FindGroupChatDialog (closes: #270)
    - Implemented Pango markup converter of a MessageModel
    - Use Pango markup within the FindGroupChatDialog for topics
    
    Signed-off-by: Mirco Bauer <meebey@meebey.net>

---
+++ src/Frontend-GNOME/PangoTools.cs
@@ -0,0 +1,95 @@
+// $Id$
+//
+// Smuxi - Smart MUltipleXed Irc
+//
+// Copyright (c) 2010 David Paleino <dapal@debian.org>
+//
+// Full GPL License: <http://www.gnu.org/licenses/gpl.txt>
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Smuxi.Engine;
+
+namespace Smuxi.Frontend.Gnome
+{
+    public class PangoTools
+    {
+        public static string ToMarkup(MessageModel Model)
+        {
+            /* Pango Markup doesn't support hyperlinks:
+             *     (smuxi-frontend-gnome:9824): Gtk-WARNING **: Failed to set
+             *     text from markup due to error parsing markup: Unknown tag
+             *     'a' on line 1 char 59
+             *
+             * For this reason, for UrlMessagePartModels, we'll render them as
+             * plaintext.
+             *
+             * Here we loop over the MessageModel to build up a proper Pango
+             * Markup.
+             *
+             * The colour codes/values have been taken from BuildTagTable(), in
+             * MessageTextView.cs.
+             *
+             * Documentation for Pango Markup is located at:
+             *    http://library.gnome.org/devel/pango/unstable/PangoMarkupFormat.html
+             */
+            StringBuilder markup = new StringBuilder ();
+            foreach (MessagePartModel msgPart in Model.MessageParts) {
+                if (msgPart is UrlMessagePartModel) {
+                    UrlMessagePartModel url = (UrlMessagePartModel) msgPart;
+
+                    string str = GLib.Markup.EscapeText(url.Text);
+                    str = String.Format("<span color='{0}'><u>{1}</u></span>",
+                                        "darkblue",
+                                        str);
+                    markup.Append(str);
+                } else if (msgPart is TextMessagePartModel) {
+                    TextMessagePartModel text = (TextMessagePartModel) msgPart;
+                    Stack<string> tags = new Stack<string>();
+
+                    string str = GLib.Markup.EscapeText(text.Text);
+                    if (text.ForegroundColor != TextColor.None) {
+                        tags.Push(String.Format("span color='#{0}'",
+                                                text.ForegroundColor.HexCode));
+                    }
+                    // TODO: do contrast checks here like we do in MessageTextView?
+                    if (text.Underline) {
+                        tags.Push("u");
+                    }
+                    if (text.Bold) {
+                        tags.Push("b");
+                    }
+                    if (text.Italic) {
+                        tags.Push("i");
+                    }
+
+                    if (tags.Count > 0) {
+                        for (int i = 0; i <= tags.Count; i++) {
+                            string tag = tags.Pop();
+                            str = String.Format("{0}{1}{2}",
+                                "<"+tag+">", str, "</"+tag.Split(' ')[0]+">");
+                        }
+                    }
+                    markup.Append(str);
+                }
+            }
+
+            return markup.ToString();
+        }
+    }
+}
