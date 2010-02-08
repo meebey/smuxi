7e83db8bb822196c2f0f121a2f01aa7d5fab0a8b
Andrius Bentkus <Andrius.Bentkus@rwth-aachen.de>
2010-02-08 17:03:28 +0100

    Initial p/invokes for STFL
    Signed-off-by: Mirco Bauer <meebey@meebey.net>

--- src/Frontend-STFL/STFL/StflApi.cs
+++ src/Frontend-STFL/STFL/StflApi.cs
@@ -8,6 +8,7 @@
  * Smuxi - Smart MUltipleXed Irc
  *
  * Copyright (c) 2005-2006 Mirco Bauer <meebey@meebey.net>
+ * Copyright (c) 2010 Andrius Bentkus <Andrius.Bentkus@rwth-aachen.de>
  *
  * Full GPL License: <http://www.gnu.org/licenses/gpl.txt>
  *
@@ -32,6 +33,73 @@ namespace Smuxi.Frontend.Stfl
 {
     internal class StlfApi
     {
-        //extern 
+        [DllImport("libstfl.so.0.21")]
+        internal static extern IntPtr stfl_create(IntPtr text);
+
+        [DllImport("libstfl.so.0.21")]
+        internal static extern void stfl_free(IntPtr form);
+
+        [DllImport("libstfl.so.0.21")]
+        internal static extern IntPtr stfl_run(IntPtr form, int timeout);
+
+        [DllImport("libstfl.so.0.21")]
+        internal static extern void stfl_reset();
+
+        [DllImport("libstfl.so.0.21")]
+        internal static extern IntPtr stfl_get(IntPtr form, IntPtr name);
+
+        [DllImport("libstfl.so.0.21")]
+        internal static extern void stfl_set(IntPtr form, IntPtr name, IntPtr val);
+
+        [DllImport("libstfl.so.0.21")]
+        internal static extern IntPtr stfl_focus(IntPtr form);
+
+        [DllImport("libstfl.so.0.21")]
+        internal static extern IntPtr stfl_quote(IntPtr text);
+
+        [DllImport("libstfl.so.0.21")]
+        internal static extern IntPtr stfl_dump(IntPtr form, IntPtr name, IntPtr prefix, int focus);
+
+        [DllImport("libstfl.so.0.21")]
+        internal static extern void stfl_modify_before(IntPtr w, IntPtr n);
+
+        [DllImport("libstfl.so.0.21")]
+        internal static extern void stfl_modify_after(IntPtr w, IntPtr n);
+
+        [DllImport("libstfl.so.0.21")]
+        internal static extern void stfl_modify_insert(IntPtr w, IntPtr n);
+
+        [DllImport("libstfl.so.0.21")]
+        internal static extern void stfl_modify_append(IntPtr w, IntPtr n);
+
+        [DllImport("libstfl.so.0.21")]
+        internal static extern void stfl_modify(IntPtr w, IntPtr n);
+
+        [DllImport("libstfl.so.0.21")]
+        internal static extern IntPtr stfl_lookup(IntPtr f, IntPtr path, IntPtr newname);
+
+        [DllImport("libstfl.so.0.21")]
+        internal static extern IntPtr stfl_error();
+
+        [DllImport("libstfl.so.0.21")]
+        internal static extern void stfl_error_action(IntPtr mode);
+
+        [DllImport("libstfl.so.0.21")]
+        internal static extern IntPtr stfl_ipool_create(IntPtr code);
+
+        [DllImport("libstfl.so.0.21")]
+        internal static extern IntPtr stfl_ipool_add(IntPtr pool, IntPtr data);
+
+        [DllImport("libstfl.so.0.21")]
+        internal static extern IntPtr stfl_ipool_towc(IntPtr pool, IntPtr buf);
+
+        [DllImport("libstfl.so.0.21")]
+        internal static extern IntPtr stfl_ipool_fromwc(IntPtr pool, IntPtr buf);
+
+        [DllImport("libstfl.so.0.21")]
+        internal static extern void stfl_ipool_flush(IntPtr pool);
+
+        [DllImport("libstfl.so.0.21")]
+        internal static extern void stfl_ipool_destroy(IntPtr pool);
     }       
 }
