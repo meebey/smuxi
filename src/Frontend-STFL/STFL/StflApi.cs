e5531fe649e75d6c500add2dd4a6cb2e3f0d3dbc
Mirco Bauer <meebey@meebey.net>
2011-02-25 22:36:02 +0100

    Use shared STFL library

--- src/Frontend-STFL/STFL/StflApi.cs
+++ src/Frontend-STFL/STFL/StflApi.cs
@@ -87,17 +87,17 @@ namespace Stfl
             return text;
         }
 
-        [DllImport("stflsharp")]
+        [DllImport("stfl")]
         static extern IntPtr stfl_create(IntPtr text);
         internal static IntPtr stfl_create(string text)
         {
             return stfl_create(ToUnixWideCharacters(text));
         }
 
-        [DllImport("stflsharp")]
+        [DllImport("stfl")]
         internal static extern void stfl_free(IntPtr form);
 
-        [DllImport("stflsharp", EntryPoint = "stfl_run")]
+        [DllImport("stfl", EntryPoint = "stfl_run")]
         static extern IntPtr stfl_run_native(IntPtr form, int timeout);
         internal static string stfl_run(IntPtr form, int timeout)
         {
@@ -108,10 +108,10 @@ namespace Stfl
             return UnixMarshal.PtrToString(res, Encoding.UTF32);
         }
 
-        [DllImport("stflsharp")]
+        [DllImport("stfl")]
         internal static extern void stfl_reset();
 
-        [DllImport("stflsharp")]
+        [DllImport("stfl")]
         static extern IntPtr stfl_get(IntPtr form, IntPtr name);
         internal static string stfl_get(IntPtr form, string text)
         {
@@ -120,7 +120,7 @@ namespace Stfl
             );
         }
 
-        [DllImport("stflsharp")]
+        [DllImport("stfl")]
         static extern void stfl_set(IntPtr form, IntPtr name, IntPtr value);
         internal static void stfl_set(IntPtr form, string name, string value)
         {
@@ -128,7 +128,7 @@ namespace Stfl
                      ToUnixWideCharacters(value));
         }
         
-        [DllImport("stflsharp", EntryPoint = "stfl_get_focus")]
+        [DllImport("stfl", EntryPoint = "stfl_get_focus")]
         static extern IntPtr stfl_get_focus_native(IntPtr form);
         internal static string stfl_get_focus(IntPtr form)
         {
@@ -139,14 +139,14 @@ namespace Stfl
             return UnixMarshal.PtrToString(res, Encoding.UTF32);
         }
         
-        [DllImport("stflsharp")]
+        [DllImport("stfl")]
         static extern void stfl_set_focus(IntPtr form, IntPtr name);
         internal static void stfl_set_focus(IntPtr form, string name)
         {
             stfl_set_focus(form, ToUnixWideCharacters(name));
         }
 
-        [DllImport("stflsharp")]
+        [DllImport("stfl")]
         static extern IntPtr stfl_quote(IntPtr text);
         internal static string stfl_quote(string text) {
             return FromUnixWideCharacters(
@@ -154,7 +154,7 @@ namespace Stfl
             );
         }
 
-        [DllImport("stflsharp")]
+        [DllImport("stfl")]
         static extern IntPtr stfl_dump(IntPtr form, IntPtr name, IntPtr prefix,
                                        int focus);
         internal static string stfl_dump(IntPtr form, string name,
@@ -166,7 +166,7 @@ namespace Stfl
             );
         }
 
-        [DllImport("stflsharp")]
+        [DllImport("stfl")]
         static extern void stfl_modify(IntPtr form, IntPtr name, IntPtr mode,
                                        IntPtr text);
         internal static void stfl_modify(IntPtr form, string name, string mode,
@@ -177,7 +177,7 @@ namespace Stfl
                         ToUnixWideCharacters(text));
         }
 
-        [DllImport("stflsharp")]
+        [DllImport("stfl")]
         static extern IntPtr stfl_lookup(IntPtr form, IntPtr path,
                                          IntPtr newname);
         internal static string stfl_lookup(IntPtr form, string path,
@@ -189,14 +189,14 @@ namespace Stfl
             );
         }
 
-        [DllImport("stflsharp", EntryPoint = "stfl_error")]
+        [DllImport("stfl", EntryPoint = "stfl_error")]
         static extern IntPtr stfl_error_native();
         internal static string stfl_error()
         {
             return FromUnixWideCharacters(stfl_error_native());
         }
 
-        [DllImport("stflsharp")]
+        [DllImport("stfl")]
         static extern void stfl_error_action(IntPtr mode);
         internal static void stfl_error_action(string mode)
         {
@@ -204,22 +204,22 @@ namespace Stfl
         }
 
         /*
-        [DllImport("stflsharp")]
+        [DllImport("stfl")]
         internal static extern IntPtr stfl_ipool_create(IntPtr code);
 
-        [DllImport("stflsharp")]
+        [DllImport("stfl")]
         internal static extern IntPtr stfl_ipool_add(IntPtr pool, IntPtr data);
 
-        [DllImport("stflsharp")]
+        [DllImport("stfl")]
         internal static extern IntPtr stfl_ipool_towc(IntPtr pool, IntPtr buf);
 
-        [DllImport("stflsharp")]
+        [DllImport("stfl")]
         internal static extern IntPtr stfl_ipool_fromwc(IntPtr pool, IntPtr buf);
 
-        [DllImport("stflsharp")]
+        [DllImport("stfl")]
         internal static extern void stfl_ipool_flush(IntPtr pool);
 
-        [DllImport("stflsharp")]
+        [DllImport("stfl")]
         internal static extern void stfl_ipool_destroy(IntPtr pool);
         */
     }       
