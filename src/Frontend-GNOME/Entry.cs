d3e3a4ea41d6ea3600a839eaeea47ff458a9c17a
Mirco Bauer <meebey@meebey.net>
2012-01-16 12:35:52 +0100

    [Fronted-GNOME] Supress page up/down scrolling in Entry

--- src/Frontend-GNOME/Entry.cs
+++ src/Frontend-GNOME/Entry.cs
@@ -389,10 +389,14 @@ namespace Smuxi.Frontend.Gnome
                     HistoryNext();
                     break;
                 case Gdk.Key.Page_Up:
+                    // supress scrolling
                     ChatViewManager.CurrentChatView.ScrollUp();
+                    e.RetVal = true;
                     break;
                 case Gdk.Key.Page_Down:
+                    // supress scrolling
                     ChatViewManager.CurrentChatView.ScrollDown();
+                    e.RetVal = true;
                     break;
                 case Gdk.Key.Return:
                     // supress adding a newline
