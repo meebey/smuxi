1e7ee8cbd066204d325ae27303963268a980b430
Mirco Bauer <meebey@meebey.net>
2012-08-20 02:53:55 +0200

    [Build] Bumped version to 0.8.11

--- src/AssemblyVersion.cs
+++ src/AssemblyVersion.cs
@@ -36,5 +36,5 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.8.10.1")]
+[assembly: AssemblyVersion("0.8.11")]
 
