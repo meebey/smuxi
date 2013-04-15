6bc56a1ba15065274fa68ce9e87fa7fe86e41c34
Will Johansson <will.johansson@gmail.com>
2013-04-14 17:00:13 -0700

    now tested. I forgot ()s around Test-Path and apparently Get-Content does not like -replace, oops.

--- msbuild-preprocess.ps1
+++ msbuild-preprocess.ps1
@@ -1,7 +1,7 @@
 $branch = ""
 $revision = ""
 
-if ($env:path.ToLower().Contains("git") -and Test-Path .git)
+if ($env:path.ToLower().Contains("git") -and (Test-Path .git))
 {
 	$branch = git symbolic-ref --short HEAD # @git_branch
 	$revision = git rev-parse HEAD # @git_commit_hash
@@ -14,6 +14,7 @@ $twitter_key_content1 = Switch -regex (Get-Content -Path configure.ac) { "with_t
 $twitter_key_content2 = $twitter_key_content1 -replace "^[^=]*=", ""
 $twitter_api_key = $twitter_key_content2 -replace "[""]", ""  # @twitter_api_key
 
-$defines = Get-Content src\Common\Defines.cs.in -replace "@git_branch@", $branch -replace "@git_commit_hash@", $revision -replace "@prefix@", "." -replace "@dist_version@", "git" -replace "@twitter_api_key@", $twitter_api_key
+$defines_in = Get-Content src\Common\Defines.cs.in 
+$defines = $defines_in -replace "@git_branch@", $branch -replace "@git_commit_hash@", $revision -replace "@prefix@", "." -replace "@dist_version@", "git" -replace "@twitter_api_key@", $twitter_api_key
 
 Set-Content src\Common\Defines.cs $defines
