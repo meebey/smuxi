957b86ea192b105aba213c54151415896e70a27c
Niichan <niichan@ponychat.net>
2013-05-24 21:23:38 -0700

    Added PonyChat to network list

--- src/Engine/Config/Config.cs
+++ src/Engine/Config/Config.cs
@@ -317,7 +317,8 @@ namespace Smuxi.Engine
                 "IRC/irc.geekshed.net",
                 "IRC/irc.efnet.org",
                 "IRC/irc.ircnet.org",
-                "IRC/irc.freenode.net"
+                "IRC/irc.freenode.net",
+                "IRC/irc.ponychat.net"
             });
             
             prefix = "Engine/Users/DEFAULT/Servers/IRC/irc.oftc.net/";
@@ -375,6 +376,15 @@ namespace Smuxi.Engine
             Get(prefix + "Hostname", "irc.freenode.net");
             Get(prefix + "Port", 6667);
             Get(prefix + "Network", "freenode");
+            Get(prefix + "Username", String.Empty);
+            Get(prefix + "Password", String.Empty);
+            Get(prefix + "UseEncryption", false);
+            Get(prefix + "ValidateServerCertificate", false);
+            
+            prefix = "Engine/Users/DEFAULT/Servers/IRC/irc.ponychat.net/";
+            Get(prefix + "Hostname", "irc.ponychat.net");
+            Get(prefix + "Port", 6667);
+            Get(prefix + "Network", "PonyChat");
             Get(prefix + "Username", String.Empty);
             Get(prefix + "Password", String.Empty);
             Get(prefix + "UseEncryption", false);
