8ba64596782d2b987b3288f88eb547a2877f692e
Mirco Bauer <meebey@meebey.net>
2013-08-19 16:34:39 +0200

    [Build] Bumped version to 0.9

--- src/AssemblyVersion.cs
+++ src/AssemblyVersion.cs
@@ -36,5 +36,5 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.8.12")]
+[assembly: AssemblyVersion("0.9")]
 
