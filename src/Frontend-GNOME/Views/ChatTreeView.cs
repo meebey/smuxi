613261a53fad22abe334cfed3bb4f776996a38be
Mirco Bauer <meebey@meebey.net>
2013-11-14 20:08:49 +0100

    [Frontend-GNOME] Fixed resorting protocol chats by name in the chat tree

--- src/Frontend-GNOME/Views/ChatTreeView.cs
+++ src/Frontend-GNOME/Views/ChatTreeView.cs
@@ -154,7 +154,11 @@ namespace Smuxi.Frontend.Gnome
 
             var iter = FindChatIter(chatView);
             var path = TreeStore.GetPath(iter);
-            TreeStore.EmitRowChanged(path, iter);
+            //TreeStore.EmitRowChanged(path, iter);
+            // HACK: this emits row_changed _and_ sort_iter_changed and there is
+            // no other public API in GTK+ to trigger a resort of a modified
+            // value in the tree view :/
+            TreeStore.SetValue(iter, 0, chatView);
         }
 
         public virtual bool IsVisible(ChatView chatView)
