meebey/smuxi

commit fc69d82cead46be445dd637100623e22213fd3ff

Engine-XMPP: send preferred nickname to contacts

src/Engine-XMPP/Protocols/Xmpp/XmppProtocolManager.cs
@@ -52,6 +52,7 @@ using Starksoft.Net.Proxy;
 
 using Smuxi.Common;
 using System.Runtime.CompilerServices;
+using agsXMPP.protocol.extensions.nickname;
 
 namespace Smuxi.Engine
 {
@@ -1328,27 +1329,7 @@ namespace Smuxi.Engine
                 if (chat.ChatType == ChatType.Person) {
                     var _person = (chat as PersonChatModel).Person as PersonModel;
                     XmppPersonModel person = GetOrCreateContact(_person.ID, _person.IdentityName);
-                    Jid jid = person.Jid;
-                    if ((jid.Server == "gmail.com") ||
-                        (jid.Server == "googlemail.com")) {
-                        // don't send to all high prio resources or to specific resources
-                        // because gtalk clones any message to all resources anyway
-                        JabberClient.Send(new Message(jid.Bare, XmppMessageType.chat, text));
-                    } else if (!String.IsNullOrEmpty(jid.Resource)) {
-                        JabberClient.Send(new Message(jid, XmppMessageType.chat, text));
-                    } else {
-                        var resources = person.GetResourcesWithHighestPriority();
-                        if (resources.Count == 0) {
-                            // no connected resource, send to bare jid
-                            JabberClient.Send(new Message(jid.Bare, XmppMessageType.chat, text));
-                        } else {
-                            foreach (var res in resources) {
-                                Jid j = new Jid(jid);
-                                j.Resource = res.Name;
-                                JabberClient.Send(new Message(j, XmppMessageType.chat, text));
-                            }
-                        }
-                    }
+                    SendPrivateMessage(person, text);
                 } else if (chat.ChatType == ChatType.Group) {
                     JabberClient.Send(new Message(chat.ID, XmppMessageType.groupchat, text));
                     return; // don't show now. the message will be echoed back if it's sent successfully
@@ -1370,6 +1351,42 @@ namespace Smuxi.Engine
             OnMessageSent(
                 new MessageEventArgs(chat, msg, null, chat.ID)
             );
+        }
+
+        void SendPrivateMessage(XmppPersonModel person, Jid jid, string text)
+        {
+            var mesg = new Message(jid, XmppMessageType.chat, text);
+            var res = person.GetOrCreateResource(jid);
+            if (res.NicknameContactKnowsFromMe != Nicknames[0]) {
+                res.NicknameContactKnowsFromMe = Nicknames[0];
+                mesg.Nickname = new Nickname(Nicknames[0]);
+            }
+            JabberClient.Send(mesg);
+        }
+
+        void SendPrivateMessage(XmppPersonModel person, string text)
+        {
+            Jid jid = person.Jid;
+            if ((jid.Server == "gmail.com") ||
+                (jid.Server == "googlemail.com")) {
+                // don't send to all high prio resources or to specific resources
+                // because gtalk clones any message to all resources anyway
+                SendPrivateMessage(person, jid.Bare, text);
+            } else if (!String.IsNullOrEmpty(jid.Resource)) {
+                SendPrivateMessage(person, jid, text);
+            } else {
+                var resources = person.GetResourcesWithHighestPriority();
+                if (resources.Count == 0) {
+                    // no connected resource, send to bare jid
+                    SendPrivateMessage(person, jid.Bare, text);
+                } else {
+                    foreach (var res in resources) {
+                        Jid j = new Jid(jid);
+                        j.Resource = res.Name;
+                        SendPrivateMessage(person, j, text);
+                    }
+                }
+            }
         }
 
         void OnReadXml(object sender, string text)

src/Engine-XMPP/Config/XmppPersonModel.cs
@@ -33,6 +33,7 @@ namespace Smuxi.Engine
         public Presence Presence { get; set; }
         public DiscoInfo Disco { get; set; }
         public string Name { get; set; }
+        public string NicknameContactKnowsFromMe { get; set; }
     }
     
     internal class XmppPersonModel : PersonModel

commit d4dcc3132907bbb431492223ba00a4d4566072fb

Engine-XMPP: regression from fc69d82cead caused NRE when sending messages to bare Jids
this was noticed in gtalk, as messages there are always sent to bare Jids.
calling /whois on a contact to which such a message had been sent caused the NullRefException.
this will be properly fixed in refactor/XmppResourceModel

src/Engine-XMPP/Protocols/Xmpp/XmppProtocolManager.cs
@@ -1358,17 +1358,21 @@ namespace Smuxi.Engine
             );
         }
 
+        [MethodImpl(MethodImplOptions.Synchronized)]
         void SendPrivateMessage(XmppPersonModel person, Jid jid, string text)
         {
             var mesg = new Message(jid, XmppMessageType.chat, text);
-            var res = person.GetOrCreateResource(jid);
-            if (res.NicknameContactKnowsFromMe != Nicknames[0]) {
-                res.NicknameContactKnowsFromMe = Nicknames[0];
-                mesg.Nickname = new Nickname(Nicknames[0]);
+            XmppResourceModel res;
+            if (person.Resources.TryGetValue(jid.Resource ?? "", out res)) {
+                if (res.NicknameContactKnowsFromMe != Nicknames[0]) {
+                    res.NicknameContactKnowsFromMe = Nicknames[0];
+                    mesg.Nickname = new Nickname(Nicknames[0]);
+                }
             }
             JabberClient.Send(mesg);
         }
 
+        [MethodImpl(MethodImplOptions.Synchronized)]
         void SendPrivateMessage(XmppPersonModel person, string text)
         {
             Jid jid = person.Jid;