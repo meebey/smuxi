a62c4e17325622f5a2fbf9c52ea64cc32453f44b
Mirco Bauer <meebey@meebey.net>
2013-12-06 22:07:46 +0100

    Frontend-GNOME: don't show desktop notifications for active chats

--- src/Frontend-GNOME/NotifyManager.cs
+++ src/Frontend-GNOME/NotifyManager.cs
@@ -219,11 +219,13 @@ namespace Smuxi.Frontend.Gnome
             if (!IsEnabled ||
                 e.Message.TimeStamp <= chatView.SyncedLastSeenHighlight ||
                 (MainWindow.HasToplevelFocus &&
-                 MainWindow.ChatTreeView.IsVisible(chatView))) {
+                 (ChatViewManager.CurrentChatView == chatView ||
+                  MainWindow.ChatTreeView.IsVisible(chatView)))) {
                 // no need to show a notification for:
                 // - disabled chats
                 // - seen highlights
-                // - main window has focus and the chat row is visible
+                // - main window has focus and the chat is active or the chat
+                //   row is visible
                 return;
             }
 
