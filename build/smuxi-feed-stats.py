96df283a13ff68ec63e5d2202d0b725101bcd6f0
Mirco Bauer <meebey@meebey.net>
2015-05-23 16:19:26 +0200

    smuxi-feed-stats.py: fixed new lines of header

--- build/smuxi-feed-stats.py
+++ build/smuxi-feed-stats.py
@@ -94,9 +94,9 @@ if __name__=='__main__':
     print """================================
 Smuxi World Domination Progress
 ===============================
-
-    """
+"""
     print "Number of unique IPs: %s"%len(unique_ips())
+    print ""
     print 'IP'.ljust(25), '| PTR'.ljust(50), '  | OS'.ljust(20), '| Program'.ljust(24), '| Version'.ljust(17), '| C'
     print "-" * 145
     log()
