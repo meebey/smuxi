meebey/smuxi

commit d7c7508ce478b7074957a8aac8f779bf3d4f7d10

Engine-IRC: link Smuxi FAQ for SSL/TLS certificate error messages

src/Engine-IRC/Protocols/Irc/IrcProtocolManager.cs
@@ -452,6 +452,16 @@ namespace Smuxi.Engine
                 builder.AppendEventPrefix();
                 builder.AppendText(_("Connection failed! Reason: "));
                 builder.AppendText(ex.Message);
+                if (ex.InnerException is IOException &&
+                    ex.InnerException.InnerException != null &&
+                    ex.InnerException.InnerException.GetType().FullName == "Mono.Security.Protocol.Tls.TlsException") {
+                    // this is a CA / certificate issue
+                    builder.AppendSpace();
+                    builder.AppendUrl(
+                        "https://smuxi.im/faq/troubleshooting/linux-tls/",
+                        "[" + _("Open Smuxi FAQ for help") + "]"
+                    );
+                }
                 Session.AddMessageToChat(Chat, builder.ToMessage());
                 throw;
             }

commit 7536131a0904104e6758e57b63175be6475ceb0a

Engine-IRC: fix build
This fixes a build regression introduced in
d7c7508ce478b7074957a8aac8f779bf3d4f7d10

src/Engine-IRC/Protocols/Irc/IrcProtocolManager.cs
@@ -21,6 +21,7 @@
  */
 
 using System;
+using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Security.Cryptography.X509Certificates;