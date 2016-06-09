946702152d731a79e3ce3e23bded3f3597f5d21e
Mirco Bauer <meebey@meebey.net>
2016-06-09 22:40:43 +0200

    Build: bump version to 1.0.5

--- src/AssemblyVersion.cs
+++ src/AssemblyVersion.cs
@@ -36,5 +36,5 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.0.4")]
+[assembly: AssemblyVersion("1.0.5")]
 
