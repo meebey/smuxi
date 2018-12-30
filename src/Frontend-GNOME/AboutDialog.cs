8e337f3c2dcc66c4e52fceeb0908f533eaf782ad
Mirco Bauer <meebey@meebey.net>
2018-12-30 18:07:25 +0100

    Frontend-GNOME: update copyright info

--- src/Frontend-GNOME/AboutDialog.cs
+++ src/Frontend-GNOME/AboutDialog.cs
@@ -46,7 +46,7 @@ namespace Smuxi.Frontend.Gnome
             if (!Frontend.IsLocalEngine) {
                 Version += "\n Protocol: " + Frontend.EngineProtocolVersion;
             }
-            Copyright = "Copyright © 2005-2017 Mirco Bauer <meebey@meebey.net> and other contributors";
+            Copyright = "Copyright © 2005-2018 Mirco Bauer <meebey@meebey.net> and other contributors";
             Authors = new string[] {
                 "Mirco Bauer <meebey@meebey.net>",
                 "David Paleino <dapal@debian.org>",
@@ -56,7 +56,8 @@ namespace Smuxi.Frontend.Gnome
                 "Bianca Mix <heavydemon@freenet.de>",
                 "Oliver Schneider <mail@oli-obk.de>",
                 "Carlos Martín Nieto <cmn@dwim.me>",
-                "Andres G. Aragoneses <knocte@gmail.com>"
+                "Andres G. Aragoneses <knocte@gmail.com>",
+                "Ondřej Hošek <ondra.hosek@gmail.com>"
             };
             Artists = new string[] {
                 "Jakub Steiner <jimmac@ximian.com>",
