63b2b8389dda1f81e403a20fe7be10e19f972f27
Andres G. Aragoneses <knocte@gmail.com>
2020-02-17 21:22:38 +0800

    CI: try to fix perl module XML::Parser missing hinted by configure
    Error is:
    checking whether NLS is requested... yes
    checking for intltool >= 0.25... 0.51.0 found
    checking for intltool-update... /usr/local/bin/intltool-update
    checking for intltool-merge... /usr/local/bin/intltool-merge
    checking for intltool-extract... /usr/local/bin/intltool-extract
    checking for xgettext... /usr/local/bin/xgettext
    checking for msgmerge... /usr/local/bin/msgmerge
    checking for msgfmt... /usr/local/bin/msgfmt
    checking for gmsgfmt... /usr/local/bin/msgfmt
    checking for perl... /usr/local/bin/perl
    checking for perl >= 5.8.1... 5.30.1
    configure: error: XML::Parser perl module is required for intltool
    checking for XML::Parser...
    ##[error]Process completed with exit code 1.

--- build/install-deps-macos.sh
+++ build/install-deps-macos.sh
@@ -8,3 +8,6 @@ brew install gettext
 
 #see https://unix.stackexchange.com/a/387362/56844
 brew link gettext --force
+
+# needed by intltool (`brew install intltool` already installs perl&cpan)
+cpan -i XML::Parser
