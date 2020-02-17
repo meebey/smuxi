209c5962674cb15b63a40b6f49c4ebef2feb232c
Mirco Bauer <meebey@meebey.net>
2020-02-17 23:09:56 +0800

    Build: bump version to 1.0.8

--- src/AssemblyVersion.cs
+++ src/AssemblyVersion.cs
@@ -36,5 +36,5 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.0.7")]
+[assembly: AssemblyVersion("1.0.8")]
 
