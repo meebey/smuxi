meebey/smuxi

commit 83a2ab1c3e64ef4438b8e901891270f65566ea95

Engine(-IRC), Frontend-GNOME: support CertFP (closes: #96)
[CertFP][] is a NickServ authentication feature supported by modern IRC networks
as an more secure alternative to the famous "/msg NickServ IDENTIFY my_password"
command.

 [CertFP]: https://freenode.net/certfp/

As this is an internal setting only (for now) you need to configure it using
the /config command like this:

    /config Servers/IRC/$SERVER_ID/ClientCertificateFilename = mycert.pfx
    /config save

The client certificate can be generated using makecert like this:

    makecert -eku 1.3.6.1.5.5.7.3.2 -r -cy end -n "CN=$USER" -p12 mycert.pfx ""

The certificate must not use a passphrase, else it can't be loaded. Thus secure
the file against access by other users with:

    chmod 400 mycert.pfx

Place the certificate in ~/.config/smuxi/certs/ otherwise specify the full path
in ClientCertificateFilename.

On most IRC networks that support CertFP you can verify if the certificate was
used using /whois on your own nickname. A line like this should show up in the
whois reply:

    [276 (?) meebey3] has client certificate fingerprint a15aecab43e1d0965a2da43739a9628d790994e0

Special thanks goes to An-Ivoz for finding out how client certificate selection
works!

src/Engine/Config/ServerModel.cs
@@ -32,6 +32,7 @@ namespace Smuxi.Engine
     {
         public bool UseEncryption { get; set; }
         public bool ValidateServerCertificate { get; set; }
+        public string ClientCertificateFilename { get; set; }
         public string Protocol { get; set; }
         public string Hostname { get; set; }
         public int Port { get; set; }
@@ -100,6 +101,9 @@ namespace Smuxi.Engine
                         ValidateServerCertificate = (bool)e.Value;
                         foundValidation = true;
                         break;
+                    case "ClientCertificateFilename":
+                        ClientCertificateFilename = (string) e.Value;
+                        break;
                 }
             }
             if (foundServerID == false) {
@@ -131,6 +135,12 @@ namespace Smuxi.Engine
             }
             if (Realname != null) {
                 info.AddValue("_Realname", Realname);
+            }
+            // HACK: skip ClientCertificateFilename if it has no value as it
+            // breaks older ServerModel implementations that relied on automatic
+            // serialization which was the case in < 0.8.11
+            if (String.IsNullOrEmpty(ClientCertificateFilename)) {
+                info.AddValue("ClientCertificateFilename", ClientCertificateFilename);
             }
             info.AddValue("_Protocol", Protocol);
             info.AddValue("_Hostname", Hostname);
@@ -175,6 +185,7 @@ namespace Smuxi.Engine
             UseEncryption = (bool) config[ConfigKeyPrefix + "UseEncryption"];
             ValidateServerCertificate =
                 (bool) config[ConfigKeyPrefix + "ValidateServerCertificate"];
+            ClientCertificateFilename = (string) config[ConfigKeyPrefix + "ClientCertificateFilename"];
             if (config[ConfigKeyPrefix + "OnStartupConnect"] != null) {
                 OnStartupConnect = (bool) config[ConfigKeyPrefix + "OnStartupConnect"];
             }
@@ -196,6 +207,8 @@ namespace Smuxi.Engine
             config[ConfigKeyPrefix + "UseEncryption"] = UseEncryption;
             config[ConfigKeyPrefix + "ValidateServerCertificate"] =
                 ValidateServerCertificate;
+            config[ConfigKeyPrefix + "ClientCertificateFilename"] =
+                ClientCertificateFilename;
             config[ConfigKeyPrefix + "OnStartupConnect"] = OnStartupConnect;
             config[ConfigKeyPrefix + "OnConnectCommands"] = OnConnectCommands;
         }

src/Engine-IRC/Protocols/Irc/IrcProtocolManager.cs
@@ -21,6 +21,7 @@
  */
 
 using System;
+using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Security.Cryptography.X509Certificates;
@@ -2584,6 +2585,41 @@ namespace Smuxi.Engine
             if (server != null) {
                 _IrcClient.UseSsl = server.UseEncryption;
                 _IrcClient.ValidateServerCertificate = server.ValidateServerCertificate;
+                if (String.IsNullOrEmpty(server.ClientCertificateFilename)) {
+                    _IrcClient.SslClientCertificate = null;
+                } else {
+                    var certFile = server.ClientCertificateFilename;
+                    if (!Path.IsPathRooted(certFile)) {
+                        var configPath = Environment.GetFolderPath(
+                            Environment.SpecialFolder.ApplicationData
+                        );
+                        configPath = Path.Combine(configPath, "smuxi");
+                        var certPath = Path.Combine(configPath, "certs");
+                        certFile = Path.Combine(certPath, certFile);
+                    }
+                    var certType = X509Certificate2.GetCertContentType(certFile);
+                    if (certType != X509ContentType.Unknown) {
+                        var cert = new X509Certificate2();
+                        cert.Import(certFile, "", X509KeyStorageFlags.PersistKeySet);
+                        if (cert.PublicKey == null) {
+#if LOG4NET
+                            _Logger.ErrorFormat(
+                                "ApplyConfig(): client certificate {0} does " +
+                                "not contain a public key!", certFile
+                            );
+#endif
+                        }
+                        if (cert.PrivateKey == null) {
+#if LOG4NET
+                            _Logger.ErrorFormat(
+                                "ApplyConfig(): client certificate {0} does " +
+                                "not contain a private key!", certFile
+                            );
+#endif
+                        }
+                        _IrcClient.SslClientCertificate = cert;
+                    }
+                }
             }
         }
 

src/Engine/Config/Config.cs
@@ -617,6 +617,7 @@ namespace Smuxi.Engine
                     LoadEntry(sprefix+"Password", String.Empty);
                     LoadEntry(sprefix+"UseEncryption", false);
                     LoadEntry(sprefix+"ValidateServerCertificate", false);
+                    LoadEntry(sprefix+"ClientCertificateFilename", String.Empty);
                     LoadEntry(sprefix+"OnStartupConnect", false);
                     string[] commands = GetList(sprefix + "OnConnectCommands");
                     if (commands == null) {

src/Frontend-GNOME/Views/MenuWidget.cs
@@ -227,6 +227,7 @@ namespace Smuxi.Frontend.Gnome
                             server.ServerID = null;
                             server.Nickname = null;
                             server.Realname = null;
+                            server.ClientCertificateFilename = null;
                         }
                         Frontend.Session.Connect(server, Frontend.FrontendManager);
                     } catch (Exception ex) {

commit 4cf007103b864d0b91d28c956b6fda368f22aeb5

Engine: fix ArgumentNullException thrown in ServerModel.Save()
When a server is serialized into the config an ArgumentNullException is thrown:

    Exception Type:
    System.ArgumentNullException

    Exception Message:
    Value cannot be null.
    Parameter name: value

    Exception StackTrace:
      at Smuxi.Engine.UserConfig.Set (System.String key, System.Object value) [0x0001a] in <f3a1234091714dbfa3231ad6d12c9efa>:0
      at Smuxi.Engine.UserConfig.set_Item (System.String key, System.Object value) [0x00001] in <f3a1234091714dbfa3231ad6d12c9efa>:0
      at (wrapper remoting-invoke-with-check) Smuxi.Engine.UserConfig.set_Item(string,object)
      at Smuxi.Engine.ServerModel.Save (Smuxi.Engine.UserConfig config) [0x00135] in <f3a1234091714dbfa3231ad6d12c9efa>:0
      at Smuxi.Engine.ServerListController.AddServer (Smuxi.Engine.ServerModel server) [0x00134] in <f3a1234091714dbfa3231ad6d12c9efa>:0
      at Smuxi.Frontend.Gnome.MenuWidget.OnAddServerActionActivated (System.Object sender, System.EventArgs e) [0x0005b] in <1b690ecd2141485a99f79b236cb55f92>:0

ServerModel.ClientCertificateFilename can be null when Smuxi frontends don't
expose the feature in the user interface, which ServerWidget of the GNOME
frontend is not exposing (yet).

This bug was introduced in 83a2ab1c3e64ef4438b8e901891270f65566ea95 when CertFP
support was added.

src/Engine/Config/ServerModel.cs
@@ -229,8 +229,10 @@ namespace Smuxi.Engine
             config[ConfigKeyPrefix + "UseEncryption"] = UseEncryption;
             config[ConfigKeyPrefix + "ValidateServerCertificate"] =
                 ValidateServerCertificate;
+            // ClientCertificateFilename is an internal setting which not
+            // exposed in the UI, thus the value remains null
             config[ConfigKeyPrefix + "ClientCertificateFilename"] =
-                ClientCertificateFilename;
+                ClientCertificateFilename ?? String.Empty;
             config[ConfigKeyPrefix + "OnStartupConnect"] = OnStartupConnect;
             config[ConfigKeyPrefix + "OnConnectCommands"] = OnConnectCommands;
         }