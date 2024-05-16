9d10469db168423743e41aa27add8cc92e6e1777
Andres G. Aragoneses <knocte@gmail.com>
2024-05-16 19:32:14 +0800

    WIP: remove workaround that might not be needed anymore

--- build/install-snapcraft.sh
+++ build/install-snapcraft.sh
@@ -8,7 +8,4 @@ snap version
 # to use SNAPCRAFT_STORE_CREDENTIALS instead of --with when pushing
 sudo snap install --classic snapcraft
 
-# workaround for GithubActionsCI+snapcraft, see https://forum.snapcraft.io/t/permissions-problem-using-snapcraft-in-azure-pipelines/13258/14?u=knocte
-sudo chown root:root /
-
 snapcraft --version
